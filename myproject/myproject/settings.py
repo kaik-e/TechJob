1431327d8b3d480bbd443645969e287eb2e67891
Davicas01 <dscpr@cesar.school>
2024-11-11 18:19:27 -0300

    Fixes #8: Corrige o erro de accounts

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tailwind',
+    'accounts',
+    
+    'django.contrib.sites',  
 ]
 
 MEDIA_URL = '/media/'
